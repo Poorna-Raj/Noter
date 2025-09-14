7aaf7c37f366c0d109de89a6d989703cf4dd65ae
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-09-15 00:20:00 +0530

    add custom window frame with utility buttons with icons

--- Noter/Main.Designer.cs
+++ Noter/Main.Designer.cs
@@ -33,13 +33,20 @@
             this.SidePanel = new System.Windows.Forms.Panel();
             this.ToolPanel = new System.Windows.Forms.Panel();
             this.ContentPanel = new System.Windows.Forms.Panel();
+            this.utilBtnPanel = new System.Windows.Forms.Panel();
+            this.btnCloseIcon = new FontAwesome.Sharp.IconButton();
+            this.btnMaxIcon = new FontAwesome.Sharp.IconButton();
+            this.btnMinIcon = new FontAwesome.Sharp.IconButton();
+            this.HeaderPanel.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.utilBtnPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // HeaderPanel
             // 
             this.HeaderPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.HeaderPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.HeaderPanel.BackColor = System.Drawing.SystemColors.WindowText;
+            this.HeaderPanel.Controls.Add(this.utilBtnPanel);
             this.HeaderPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.HeaderPanel.Location = new System.Drawing.Point(0, 0);
             this.HeaderPanel.Name = "HeaderPanel";
@@ -85,6 +92,68 @@
             this.ContentPanel.Size = new System.Drawing.Size(810, 607);
             this.ContentPanel.TabIndex = 1;
             // 
+            // utilBtnPanel
+            // 
+            this.utilBtnPanel.Controls.Add(this.btnMinIcon);
+            this.utilBtnPanel.Controls.Add(this.btnMaxIcon);
+            this.utilBtnPanel.Controls.Add(this.btnCloseIcon);
+            this.utilBtnPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.utilBtnPanel.Location = new System.Drawing.Point(961, 0);
+            this.utilBtnPanel.Name = "utilBtnPanel";
+            this.utilBtnPanel.Size = new System.Drawing.Size(103, 47);
+            this.utilBtnPanel.TabIndex = 4;
+            // 
+            // btnCloseIcon
+            // 
+            this.btnCloseIcon.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnCloseIcon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseIcon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCloseIcon.IconChar = FontAwesome.Sharp.IconChar._0;
+            this.btnCloseIcon.IconColor = System.Drawing.Color.White;
+            this.btnCloseIcon.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCloseIcon.IconSize = 16;
+            this.btnCloseIcon.Location = new System.Drawing.Point(68, 3);
+            this.btnCloseIcon.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.btnCloseIcon.Name = "btnCloseIcon";
+            this.btnCloseIcon.Size = new System.Drawing.Size(32, 41);
+            this.btnCloseIcon.TabIndex = 0;
+            this.btnCloseIcon.UseVisualStyleBackColor = false;
+            this.btnCloseIcon.Click += new System.EventHandler(this.btnCloseIcon_Click);
+            // 
+            // btnMaxIcon
+            // 
+            this.btnMaxIcon.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnMaxIcon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaxIcon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMaxIcon.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnMaxIcon.IconColor = System.Drawing.Color.White;
+            this.btnMaxIcon.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMaxIcon.IconSize = 16;
+            this.btnMaxIcon.Location = new System.Drawing.Point(38, 3);
+            this.btnMaxIcon.Name = "btnMaxIcon";
+            this.btnMaxIcon.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.btnMaxIcon.Size = new System.Drawing.Size(32, 39);
+            this.btnMaxIcon.TabIndex = 3;
+            this.btnMaxIcon.UseVisualStyleBackColor = false;
+            this.btnMaxIcon.Click += new System.EventHandler(this.btnMaxIcon_Click);
+            // 
+            // btnMinIcon
+            // 
+            this.btnMinIcon.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnMinIcon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinIcon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMinIcon.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnMinIcon.IconColor = System.Drawing.Color.White;
+            this.btnMinIcon.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMinIcon.IconSize = 16;
+            this.btnMinIcon.Location = new System.Drawing.Point(3, 4);
+            this.btnMinIcon.Name = "btnMinIcon";
+            this.btnMinIcon.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.btnMinIcon.Size = new System.Drawing.Size(32, 39);
+            this.btnMinIcon.TabIndex = 4;
+            this.btnMinIcon.UseVisualStyleBackColor = false;
+            this.btnMinIcon.Click += new System.EventHandler(this.btnMinIcon_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -95,7 +164,9 @@
             this.Controls.Add(this.HeaderPanel);
             this.Name = "Main";
             this.Text = "Noter";
+            this.HeaderPanel.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
+            this.utilBtnPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -107,6 +178,10 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel ToolPanel;
         private System.Windows.Forms.Panel ContentPanel;
+        private System.Windows.Forms.Panel utilBtnPanel;
+        private FontAwesome.Sharp.IconButton btnCloseIcon;
+        private FontAwesome.Sharp.IconButton btnMaxIcon;
+        private FontAwesome.Sharp.IconButton btnMinIcon;
     }
 }
 
--- Noter/Main.cs
+++ Noter/Main.cs
@@ -7,14 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Noter
 {
     public partial class Main : Form
     {
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HTCAPTION = 0x2;
+
         public Main()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+            this.Text = string.Empty;
+            // hook MouseDown on header
+            this.HeaderPanel.MouseDown += HeaderPanel_MouseDown;
+            //load icons
+            loadIcons();
+        }
+
+        private void HeaderPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void btnCloseIcon_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void loadIcons()
+        {
+            btnCloseIcon.IconChar = FontAwesome.Sharp.IconChar.WindowClose;
+            btnMaxIcon.IconChar = FontAwesome.Sharp.IconChar.WindowMaximize;
+            btnMinIcon.IconChar = FontAwesome.Sharp.IconChar.WindowMinimize;
+        }
+
+        private void btnMaxIcon_Click(object sender, EventArgs e)
+        {
+            this.WindowState = (this.WindowState == FormWindowState.Maximized) ? FormWindowState.Normal : FormWindowState.Maximized;
+        }
+
+        private void btnMinIcon_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
