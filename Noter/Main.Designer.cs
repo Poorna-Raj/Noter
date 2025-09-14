421a33e424fd712277350c5ec906c6a391eab31f
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2025-09-14 23:06:04 +0530

    Change main page layout

---
+++ Noter/Main.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Noter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeaderPanel = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.ToolPanel = new System.Windows.Forms.Panel();
+            this.ContentPanel = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // HeaderPanel
+            // 
+            this.HeaderPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.HeaderPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.HeaderPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HeaderPanel.Location = new System.Drawing.Point(0, 0);
+            this.HeaderPanel.Name = "HeaderPanel";
+            this.HeaderPanel.Size = new System.Drawing.Size(1064, 47);
+            this.HeaderPanel.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.Controls.Add(this.ContentPanel);
+            this.panel3.Controls.Add(this.ToolPanel);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(254, 47);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(810, 634);
+            this.panel3.TabIndex = 2;
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.SidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SidePanel.Location = new System.Drawing.Point(0, 47);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(254, 634);
+            this.SidePanel.TabIndex = 1;
+            // 
+            // ToolPanel
+            // 
+            this.ToolPanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ToolPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ToolPanel.Location = new System.Drawing.Point(0, 0);
+            this.ToolPanel.Name = "ToolPanel";
+            this.ToolPanel.Size = new System.Drawing.Size(810, 27);
+            this.ToolPanel.TabIndex = 0;
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ContentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ContentPanel.Location = new System.Drawing.Point(0, 27);
+            this.ContentPanel.Name = "ContentPanel";
+            this.ContentPanel.Size = new System.Drawing.Size(810, 607);
+            this.ContentPanel.TabIndex = 1;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.SidePanel);
+            this.Controls.Add(this.HeaderPanel);
+            this.Name = "Main";
+            this.Text = "Noter";
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel HeaderPanel;
+        private System.Windows.Forms.Panel SidePanel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel ToolPanel;
+        private System.Windows.Forms.Panel ContentPanel;
+    }
+}
+
